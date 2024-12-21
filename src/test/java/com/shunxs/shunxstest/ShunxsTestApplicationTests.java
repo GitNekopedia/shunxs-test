0c97362ac694d0d83bd0345a199cb14e0109733b
Hao Zhang <hzha0917@uni.sydney.edu.au>
2024-12-21 13:28:41 +0800

    Initial commit

---
+++ src/test/java/com/shunxs/shunxstest/ShunxsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shunxs.shunxstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShunxsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
